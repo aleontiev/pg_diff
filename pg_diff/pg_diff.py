f3f038e2a5036eb9666d5af06d5a284fe5bc3c44
mrocheleau <mike.rocheleau@freshgrade.com>
2022-03-21 12:25:34 -0700

    Fixing total relation size for pg12

--- pg_diff/pg_diff.py
+++ pg_diff/pg_diff.py
@@ -486,7 +486,7 @@ class DBTableTotalSizeDiff(DBDiffBase):
     select
       table_schema,
       table_name,
-      pg_total_relation_size(table_name)
+      pg_total_relation_size(quote_ident(table_name))
     from information_schema.tables
     where
       table_schema not in {}
